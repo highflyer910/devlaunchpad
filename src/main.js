66d0ff0373d9d74676540e65597bb501e0e8efbc
Thea Mushambadze <thea.mushambadze@gmail.com>
2025-07-01 00:15:59 +0400

    add analytics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { inject } from "@vercel/analytics"
+
+inject()
 
 const app = createApp(App)
 const pinia = createPinia()
